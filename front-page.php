15fda3096fa29f1abec61f15416217bbf57353f7
Gustav Zimmer <gustav.zimmer@hotmail.com>
2025-10-24 09:16:58 +0200

    Made changes to cookiebot style so its not in the way of nav in mobile view

--- front-page.php
+++ front-page.php
@@ -1,12 +1,13 @@
 <?php
     /* Template Name: Startsida – special */
     get_header()?>
-    <main class="flex items-center h-[30vh] pt-[2rem] mx-auto">
-        <div class="frontpage-title contain">
+    <main class="flex items-center h-[30vh] pt-[2rem] mx-auto frontpageContainer contain">
+        <div class="frontpage-title">
             <h1 class="h1Small">Gustav Zimmer</h1>
-            <h2 class="w-[8ch]">Frontend Developer</h2>
+            <h2 class="">Frontend Developer</h2>
         </div>
 
     </main>
 
 <?php get_footer()?>
+
